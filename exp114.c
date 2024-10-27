ecef1621709fe425d7e090f5eec3d2705842faa9
KRISH141006 <krish.sondagar129602@marwadiuniversity.ac.in>
2024-10-27 18:37:01 +0530

    exps

--- exp114.c
+++ exp114.c
@@ -3,12 +3,12 @@
 int main()
 {
     int n[5];
-    printf("Enter the number:-");
+    printf("Enter the number : ");
     for(int i=0;i<5;i++)
     {
         scanf("%d",&n[i]);
     }
-    printf("numbers in array");
+    printf("numbers in array:-\n");
     for(int i=0;i<5;i++)
     {
         printf("%d\n",n[i]);
